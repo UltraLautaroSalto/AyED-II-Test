5fa724ab23301f9e74ecc2d19430f76829e488bf
UltraLautaroSalto <101681380+UltraLautaroSalto@users.noreply.github.com>
2023-04-23 17:18:51 -0300

    Add files via upload

---
+++ Proyecto/dos.cpp
@@ -0,0 +1,12 @@
+// Your First C++ Program
+
+#include <iostream>
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    cout << "Dos cpp";
+        cout << "Duplico desde GitHub diferencia";
+    return 0;
+}
---
+++ Proyecto/uno.cpp
@@ -0,0 +1,12 @@
+// Your First C++ Program
+
+#include <iostream>
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    cout << "Hola Mundo AyED II";
+        cout << "Hola Mundo AyED II";
+    return 0;
+}
